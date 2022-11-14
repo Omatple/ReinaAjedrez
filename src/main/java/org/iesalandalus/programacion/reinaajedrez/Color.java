63dfe63acdab42b44a31addd120066255f80e014
hermo <hermo@DESKTOP-9SUJSB4>
2022-11-14 22:53:21 +0100

    .

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/Color.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/Color.java
@@ -1,13 +1,14 @@
 package org.iesalandalus.programacion.reinaajedrez;
 
 public enum Color {
-	BLANCO, NEGRO;
+	BLANCO("BLANCO"), NEGRO("NEGRO");
 
 	private String cadenaAMostrar;
 
 	public Color(String cadenaAMostrar) {
 		this.cadenaAMostrar = cadenaAMostrar;
 	}
+
 	@Override
 	public String toString() {
 		return "cadenaAMostrar = " + cadenaAMostrar;
--- src/main/java/org/iesalandalus/programacion/reinaajedrez/Reina.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/Reina.java
@@ -1,6 +1,27 @@
 package org.iesalandalus.programacion.reinaajedrez;
 
 public class Reina {
-private Color color;
-private Posicion posicion;
+	public Color color;
+	private Posicion posicion;
+	
+	public Reina() {
+		this.color = ;
+		posicion
+	}
+	public Color getColor() {
+		return color;
+	}
+
+	private void setColor(Color color) {
+		this.color = color;
+	}
+
+	public Posicion getPosicion() {
+		return posicion;
+	}
+
+	private void setPosicion(Posicion posicion) {
+		this.posicion = posicion;
+	}
+	
 }
