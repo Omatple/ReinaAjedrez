caf20747d444c5be236615abb2a8937fb21c9995
diurno <diurno@pc13>
2022-11-14 14:40:36 +0100

    Constructor

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/Color.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/Color.java
@@ -2,15 +2,13 @@ package org.iesalandalus.programacion.reinaajedrez;
 
 public enum Color {
 	BLANCO, NEGRO;
+
 	String cadenaAMostrar;
 
-	public String getCadenaAMostrar() {
-		return cadenaAMostrar;
-	}
-
-	public void setCadenaAMostrar(String cadenaAMostrar) {
+	public void CadenaAMostrar() {
 		this.cadenaAMostrar = cadenaAMostrar;
 	}
+
 	@Override
 	public String toString() {
 		return "Cadena -> " + cadenaAMostrar;
