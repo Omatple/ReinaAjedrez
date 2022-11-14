507f1608b471e83d92c4b29dbdf41f04363927b9
hermo <hermo@DESKTOP-9SUJSB4>
2022-11-14 18:58:06 +0100

    Creados los métodos get y set para los atributos. El método set se debe tener en cuenta que los valores indicados sean correctos (las filas van del 1 al 8, y las columnas de la 'a' a la 'h') y si no se lanza una excepción del tipo IllegalArgumentException.

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/Posicion.java
@@ -3,4 +3,32 @@ package org.iesalandalus.programacion.reinaajedrez;
 public class Posicion {
 	private int fila;
 	private char columna;
+
+	public int getFila() {
+		return fila;
+	}
+
+	public void setFila(int fila) {
+		if (fila < 1) {
+			throw new IllegalArgumentException("Esta casilla no existe, ni ninguna por debajo de la casilla 1. ");
+		} else if (fila > 8) {
+			throw new IllegalArgumentException("Esta casilla no existe, ni ninguna por encima de la casilla 8. ");
+		}
+		this.fila = fila;
+	}
+
+	public char getColumna() {
+		return columna;
+	}
+
+	public void setColumna(char columna) {
+		char a = 1;
+		char h = 8;
+		if (columna < a) {
+			throw new IllegalArgumentException("Esta casilla no existe, ni ninguna por debajo de la casilla a. ");
+		} else if (columna > h) {
+			throw new IllegalArgumentException("Esta casilla no existe, ni ninguna por encima de la casilla h. ");
+		}
+		this.columna = columna;
+	}
 }
